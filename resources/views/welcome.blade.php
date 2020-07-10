12e3c18d5ca4178d464e8f4cc5feed4002b3ab28
Wahyu Ramadhan <ssjrama@gmail.com>
2020-07-10 15:09:51 +0700

    Update carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,47 +6,32 @@
         <div class="loader"></div>
     </div>
 
-<!-- Hero Slider Begin -->
-    <section class="hero-slider">
-        <div class="hero-items owl-carousel">
-            <div class="single-slider-item set-bg" data-setbg="images/dasboard.jpg">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <h1>Harum Hijab</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="single-slider-item set-bg" data-setbg="img/slider-1.jpg">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                           <h1>Harum Hijab</h1> 
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="single-slider-item set-bg" data-setbg="img/slider-2.jpg">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <h1>Harum Hijab</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="single-slider-item set-bg" data-setbg="img/slider-3.jpg">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <h1>Harum Hijab</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="images/dasboard.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="img/slider-1.jpg" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="img/slider-2.jpg" alt="Third slide">
+          </div>
         </div>
-    </section>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
     <!-- Hero Slider End -->
 
     <!-- Latest Product Begin -->
